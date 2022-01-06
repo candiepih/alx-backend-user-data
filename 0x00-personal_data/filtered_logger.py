2ca0a936a4360bbcd74a48ccdad376b8fa6736a9
Candiepih <mutheeal.am@gmail.com>
2022-01-06 11:11:20 +0300

    Modified get_logger method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,7 +46,8 @@ def get_logger() -> logging.Logger:
     logger.setLevel(logging.INFO)
     logger.propagate = False
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    log_format = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(log_format)
     logger.addHandler(stream_handler)
     return logger
 
