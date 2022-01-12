b4dfcd86fc45a8c359bb000658d7e6f269954560
Candiepih <mutheeal.am@gmail.com>
2022-01-12 10:47:06 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains class SessionAuth that handles session authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Handles session authentication.
+    """
+    pass
