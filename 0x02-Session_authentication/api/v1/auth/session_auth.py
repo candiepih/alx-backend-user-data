90b0bdbddb48fe6526dd926ff562e3220e548930
Candiepih <mutheeal.am@gmail.com>
2022-01-12 11:12:02 +0300

    Session cookie update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,43 @@
 Contains class SessionAuth that handles session authentication.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Handles session authentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session.
+
+        Args:
+            user_id: The user id.
+
+        Returns:
+            The session id.
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns the user id for the session id.
+
+        Args:
+            session_id: The session id.
+
+        Returns:
+            The user id.
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+
