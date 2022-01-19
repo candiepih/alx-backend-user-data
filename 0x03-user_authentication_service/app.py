97db9e484878c85be73dc08719439a1d699b5e62
Candiepih <mutheeal.am@gmail.com>
2022-01-19 15:36:13 +0300

    updated PUT /reset_password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,8 +100,11 @@ def update_password() -> tuple:
     Reset password with token route
     """
     email = request.form.get('email')
-    new_password = request.form.get('new_password')
     reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
+        abort(403)
 
     try:
         AUTH.update_password(reset_token, new_password)
