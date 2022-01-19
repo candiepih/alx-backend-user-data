b3297fe8a9d1dcc9a969826481c812945320dc20
Candiepih <mutheeal.am@gmail.com>
2022-01-19 15:32:17 +0300

    updated PUT /reset_password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -105,10 +105,10 @@ def update_password() -> tuple:
 
     try:
         AUTH.update_password(reset_token, new_password)
-        res = {"email": email, "message": "Password updated"}
-        return jsonify(res), 200
-    except Exception:
+    except ValueError:
         abort(403)
+    res = {"email": email, "message": "Password updated"}
+    return jsonify(res), 200
 
 
 if __name__ == "__main__":
