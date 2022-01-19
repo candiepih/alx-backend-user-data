ce09110e02f23b433aa9d29ce877c29d31c252c9
Candiepih <mutheeal.am@gmail.com>
2022-01-19 13:46:35 +0300

    Fix to correct typing annotations

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,7 +35,7 @@ def register_user() -> Union[str, tuple]:
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login_user() -> Union[str, tuple]:
+def login_user() -> str:
     """
     Login user route
     """
